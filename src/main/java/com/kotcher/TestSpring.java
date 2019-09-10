c4b56e5dbfcc962ead321ecec00504dfe60659b8
KotcherP <chernobai88@gmail.com>
2019-09-10 10:55:44 +0300

    Initial commit

---
+++ src/main/java/com/kotcher/TestSpring.java
@@ -0,0 +1,21 @@
+package com.kotcher;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+
+    public static void main(String[] args) {
+        //класс помещает все beans ,которые описаны в конфигурационном файле,в контекст спринга.
+        //важно,чтобы файл был xml и в папке resourse (помеченная,как ресурсная)
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+
+        //testBean - id бина (бин = объект класса)
+        TestBean testBean = context.getBean("testBean",TestBean.class);
+
+        System.out.println(testBean.getName());
+
+        context.close();
+    }
+}
